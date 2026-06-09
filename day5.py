bd46402000800ee414617f0b4c004b072bebeebb
tanvirnetsquaresoftware-rgb <tanvir.netsquaresoftware@gmail.com>
2026-06-09 17:11:32 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,6 +1,7 @@
 from resume_reader import extracting_resume
 from resume_analyser import analyse
 from improver import improve_resume
+from evaluator import evaluate_resume
 
 resume_text=extracting_resume(r"C:\Users\Tanul\OneDrive\Desktop\Manan Resume  (1).pdf")
 
@@ -8,4 +9,10 @@ analysis=analyse(resume_text)
 
 improved=improve_resume(resume_text,analysis)
 
-print(improved)
+comparison=evaluate_resume(
+    analysis,
+    improved
+)
+
+
+print(comparison)
